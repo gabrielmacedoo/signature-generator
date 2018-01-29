1c8f95dabb1aefd2287d28b416d7c9fbd3bbd3ec
Gabriel Macedo <dev@PinnacleAdmins-MacBook-Pro.local>
2018-01-29 14:35:09 -0500

    added new file name when saved

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (!empty($_REQUEST['Sender'])):
     if (!empty($_REQUEST['download'])) {
         header('Content-Description: File Transfer');
         header('Content-Type: text/html');
-        header('Content-Disposition: attachment; filename=assinatura.html');
+        header('Content-Disposition: attachment; filename=signature.html');
         header('Content-Transfer-Encoding: binary');
         header('Expires: 0');
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
